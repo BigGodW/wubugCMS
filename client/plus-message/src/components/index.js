2b7522f1f91930114bcaa483b77d84a5640bf719
明空 <867528315@qq.com>
2024-08-17 00:10:40 +0800

    [新增功能] 优化在线留言

--- client/plus-message/src/components/index.js
+++ client/plus-message/src/components/index.js
@@ -1,10 +1,6 @@
 import {createApp} from 'vue';
-import Button from "./Button/Button.vue";
 import Message from "./Message/Message.vue";
 window.CHAN = {
-  button: (id, props) => {
-   createApp(Button, props).mount(id);
-  },
   message: (id, props) => {
     createApp(Message, props).mount(id);
   }
